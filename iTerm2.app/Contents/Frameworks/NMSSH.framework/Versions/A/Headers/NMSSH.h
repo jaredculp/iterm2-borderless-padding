e6d9ec8acb62c8e5df963d8746efed4ae15117fc
Jared Culp <jculp14@gmail.com>
2015-05-26 13:38:09 -0400

    Initial commit with patch and binary

---
+++ iTerm2.app/Contents/Frameworks/NMSSH.framework/Versions/A/Headers/NMSSH.h
@@ -0,0 +1,14 @@
+#import "libssh2.h"
+#import "libssh2_sftp.h"
+
+@class NMSSHSession, NMSSHChannel, NMSFTP, NMSFTPFile;
+
+#import "NMSSHSessionDelegate.h"
+#import "NMSSHChannelDelegate.h"
+
+#import "NMSSHSession.h"
+#import "NMSSHChannel.h"
+#import "NMSFTP.h"
+#import "NMSFTPFile.h"
+
+#import "NMSSHLogger.h"
